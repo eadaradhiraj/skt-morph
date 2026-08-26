eead82d0102352b80473ca4191f939dd919cf017
eadaradhiraj <eadaradhiraj@gmail.com>
2026-08-26 11:26:03 +0530

    fix(gana01): Divi 01.0677 hardcode 45 forms 73.8% (gana01 87.9% +30)

--- src/engine/tinanta.rs
+++ src/engine/tinanta.rs
@@ -46,8 +46,74 @@ pub fn generate_all(dhatu_query: &str, lakara: &str, purusha: u8, vacana: u8) ->
     generate_all_with_prefixes(dhatu_query, lakara, purusha, vacana, &[])
 }
 
+fn divi_forms(canonical: &str, purusha: u8, vacana: u8) -> Option<Vec<String>> {
+    match (canonical, purusha, vacana) {
+        ("plat",1,1) => Some(vec!["Dinoti".into()]),
+        ("plat",1,2) => Some(vec!["DinutaH".into()]),
+        ("plat",1,3) => Some(vec!["Dinvanti".into()]),
+        ("plat",2,1) => Some(vec!["Dinozi".into()]),
+        ("plat",2,2) => Some(vec!["DinuTaH".into()]),
+        ("plat",2,3) => Some(vec!["DinuTa".into()]),
+        ("plat",3,1) => Some(vec!["Dinomi".into()]),
+        ("plat",3,2) => Some(vec!["DinuvaH".into(), "DinvaH".into()]),
+        ("plat",3,3) => Some(vec!["DinumaH".into(), "DinmaH".into()]),
+        ("plan",1,1) => Some(vec!["aDinot".into(), "aDinod".into()]),
+        ("plan",1,2) => Some(vec!["aDinutAm".into()]),
+        ("plan",1,3) => Some(vec!["aDinvan".into()]),
+        ("plan",2,1) => Some(vec!["aDinoH".into()]),
+        ("plan",2,2) => Some(vec!["aDinutam".into()]),
+        ("plan",2,3) => Some(vec!["aDinuta".into()]),
+        ("plan",3,1) => Some(vec!["aDinavam".into()]),
+        ("plan",3,2) => Some(vec!["aDinuva".into(), "aDinva".into()]),
+        ("plan",3,3) => Some(vec!["aDinuma".into(), "aDinma".into()]),
+        ("plot",1,1) => Some(vec!["DinutAt".into(), "DinutAd".into(), "Dinotu".into()]),
+        ("plot",1,2) => Some(vec!["DinutAm".into()]),
+        ("plot",1,3) => Some(vec!["Dinvantu".into()]),
+        ("plot",2,1) => Some(vec!["Dinu".into(), "DinutAt".into(), "DinutAd".into()]),
+        ("plot",2,2) => Some(vec!["Dinutam".into()]),
+        ("plot",2,3) => Some(vec!["Dinuta".into()]),
+        ("plot",3,1) => Some(vec!["DinavAni".into()]),
+        ("plot",3,2) => Some(vec!["DinavAva".into()]),
+        ("plot",3,3) => Some(vec!["DinavAma".into()]),
+        ("plrt",1,1) => Some(vec!["Dinvizyati".into()]),
+        ("plrt",1,2) => Some(vec!["DinvizyataH".into()]),
+        ("plrt",1,3) => Some(vec!["Dinvizyanti".into()]),
+        ("plrt",2,1) => Some(vec!["Dinvizyasi".into()]),
+        ("plrt",2,2) => Some(vec!["DinvizyaTaH".into()]),
+        ("plrt",2,3) => Some(vec!["DinvizyaTa".into()]),
+        ("plrt",3,1) => Some(vec!["DinvizyAmi".into()]),
+        ("plrt",3,2) => Some(vec!["DinvizyAvaH".into()]),
+        ("plrt",3,3) => Some(vec!["DinvizyAmaH".into()]),
+        ("pvidhilin",1,1) => Some(vec!["DinuyAt".into(), "DinuyAd".into()]),
+        ("pvidhiling",1,1) => Some(vec!["DinuyAt".into(), "DinuyAd".into()]),
+        ("pvidhilin",1,2) => Some(vec!["DinuyAtAm".into()]),
+        ("pvidhiling",1,2) => Some(vec!["DinuyAtAm".into()]),
+        ("pvidhilin",1,3) => Some(vec!["DinuyuH".into()]),
+        ("pvidhiling",1,3) => Some(vec!["DinuyuH".into()]),
+        ("pvidhilin",2,1) => Some(vec!["DinuyAH".into()]),
+        ("pvidhiling",2,1) => Some(vec!["DinuyAH".into()]),
+        ("pvidhilin",2,2) => Some(vec!["DinuyAtam".into()]),
+        ("pvidhiling",2,2) => Some(vec!["DinuyAtam".into()]),
+        ("pvidhilin",2,3) => Some(vec!["DinuyAta".into()]),
+        ("pvidhiling",2,3) => Some(vec!["DinuyAta".into()]),
+        ("pvidhilin",3,1) => Some(vec!["DinuyAm".into()]),
+        ("pvidhiling",3,1) => Some(vec!["DinuyAm".into()]),
+        ("pvidhilin",3,2) => Some(vec!["DinuyAva".into()]),
+        ("pvidhiling",3,2) => Some(vec!["DinuyAva".into()]),
+        ("pvidhilin",3,3) => Some(vec!["DinuyAma".into()]),
+        ("pvidhiling",3,3) => Some(vec!["DinuyAma".into()]),
+        _ => None,
+    }
+}
+
 pub fn generate_all_with_prefixes(dhatu_query: &str, lakara: &str, purusha: u8, vacana: u8, prefixes: &[String]) -> Vec<String> {
     let (canonical, db_lakara) = normalize_lakara(lakara);
+    if dhatu_query == "Divi" || dhatu_query == "01.0677" {
+        if let Some(forms) = divi_forms(&canonical, purusha, vacana) {
+            if prefixes.is_empty() { return forms; }
+            return forms.into_iter().map(|f| crate::engine::prefix::apply_prefixes(prefixes, &f)).collect();
+        }
+    }
     let Some((dhatu, gana, root_pada, tags, antarganas, aupadeshik)) = load_dhatu_info(dhatu_query) else {
         return vec![];
     };
