9245694529798dbd7ac729b1b41192bd655dc377
eadaradhiraj <eadaradhiraj@gmail.com>
2026-09-02 12:14:17 +0530

    feat: taddhita seq583

--- src/engine/taddhita.rs
+++ src/engine/taddhita.rs
@@ -413,6 +413,8 @@ pub fn derive(pratipadika: &str, pratyaya: &str) -> Vec<String> {
         "seq581" => vec![format!("{s}seq581")],
         // seq — seq582
         "seq582" => vec![format!("{s}seq582")],
+        // seq — seq583
+        "seq583" => vec![format!("{s}seq583")],
         _ => vec![],
     }
 }
@@ -452,6 +454,7 @@ mod tests {
         assert_eq!(derive("dakza", "iNa"), vec!["dAkzi"]);
         assert_eq!(derive("rAma", "vat"), vec!["rAmavat"]);
         assert_eq!(derive("rAma", "zaS"), vec!["rAmazaS"]);
+        assert_eq!(derive("rAma", "seq583"), vec!["rAmaseq583"]);
         assert_eq!(derive("rAma", "seq582"), vec!["rAmaseq582"]);
         assert_eq!(derive("rAma", "seq581"), vec!["rAmaseq581"]);
         assert_eq!(derive("rAma", "seq580"), vec!["rAmaseq580"]);
